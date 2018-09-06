569b2381c98178b641f4134d744aa2cfb211daa0
jayseryu <dgkim723@hotmail.com>
2018-09-06 15:18:25 +0900

    2018-09-06 add rest api 'consult'

--- consult/models.py
+++ consult/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Consult(models.Model):
+    name = models.CharField(max_length=16)
+    position = models.CharField(max_length=16)
+    group = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=12)
+    describe = models.TextField()
+    file = models.FileField()
+    create_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'Consult'
--- consult/views.py
+++ consult/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from consult.models import Consult
+from consult.serializers import ConsultSerializer
 
-# Create your views here.
+
+class ConsultViewSet(viewsets.ModelViewSet):
+    queryset = Consult.objects.all()
+    serializer_class = ConsultSerializer
