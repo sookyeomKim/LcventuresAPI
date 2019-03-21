b4d9de71dff45dc9bfeb471945d7838608e98890
kimsookyeom <ikks06luck@naver.com>
2019-03-21 15:34:42 +0900

    [update] fix urls

--- inquiry/api/urls.py
+++ inquiry/api/urls.py
@@ -1,12 +1,11 @@
-from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
 from api.views import ApiViewSet
 
 router = routers.DefaultRouter()
-router.register(r'', ApiViewSet, basename='inquiry')
+router.register(r'inquiry', ApiViewSet, basename='inquiry')
 
 urlpatterns = [
-    path('inquiry', include(router.urls))
+    path('', include(router.urls))
 ]
--- inquiry/inquiry/urls.py
+++ inquiry/inquiry/urls.py
@@ -1,11 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
-
-from api.views import ApiViewSet
-
-router = routers.DefaultRouter()
-router.register(r'inquiry', ApiViewSet, basename='inquiry')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
